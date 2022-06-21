d256faea4aba4b1d561c8ce2a2d8116e876ab2ba
Ilana13N <105912527+Ilana13N@users.noreply.github.com>
2022-06-21 20:11:23 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+  for(var i = 0; i<marks.length; i++) { 
+    if(marks[i]>= 45) { 
+      console.log(marks[i]) } } }
+
+
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
